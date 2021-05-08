76a68c6d84287c085e5a5f7bcb9708e3e3e18844
Geoff Stilwell <gstilwell@gmail.com>
2021-05-07 23:43:39 -0400

    project setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+pub struct Pong;
+
+impl SimpleState for Pong {}
+
+fn main() -> amethyst::Result<()> {
+    let app_root = application_root_dir().unwrap();
+    let display_config_path = app_root.join("config/display.ron");
+    let assets_dir = app_root.join("assets/");
+    Ok(())
 }
